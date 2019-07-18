c8e8d3d42426ff2cf8a92876fe20ea3be9fcfd82
Bjorn Lindi <blindij@users.noreply.github.com>
2019-07-18 09:24:27 +0200

    Test boost::filesystem features:
    - exists()
    - is_regular_file()
    
    Path used by is_directory() changed to relative .git.

--- test/BoostKataTest.cpp
+++ test/BoostKataTest.cpp
@@ -20,14 +20,34 @@ TEST_GROUP(Boost_Filesystem){
    }
 };
 
+TEST(Boost_Filesystem, StatusQueryExists){
+   char const *p = "../.gitignore";
+   CHECK_TRUE(exists(p));
+}
+
+TEST(Boost_Filesystem, StatusQueryNotExists){
+   char const *p = "ABRAKADABRA";
+   CHECK_FALSE(exists(p));
+}
+
+TEST(Boost_Filesystem, StatusQueryNotIsRegularFile){
+   char const *p = "../.git";
+   CHECK_FALSE(is_regular_file(p));
+}
+
+TEST(Boost_Filesystem, StatusQueryIsRegularFile){
+   char const *p = "../.gitignore";
+   CHECK_TRUE(is_regular_file(p));
+}
+
 TEST(Boost_Filesystem, TestExistence){
-   char const *ps = "/Users/bjornlin/projects/boostkata";
+   char const *ps = "../.git";
    path p(ps);
    CHECK_TRUE(is_directory(p));
 }
 
 TEST(Boost_Filesystem, TestContents){
-   char const *ps = "/Users/bjornlin/projects/boostkata";
+   char const *ps = "../.git";
    path p(ps);
    if (is_directory(p)){
       for (auto x : directory_iterator(p))
@@ -35,7 +55,7 @@ TEST(Boost_Filesystem, TestContents){
       std::sort(v.begin(),v.end());
       for (auto x : v )
          outstr << x.filename() << " ";
-      CHECK_EQUAL("\".build\" \".gitignore\" \"CMakeLists.txt\" \"LICENSE\" \"README.md\" \"include\" \"lib\" \"src\" \"test\" ", outstr.str());
+      CHECK_EQUAL("\"COMMIT_EDITMSG\" \"HEAD\" \"config\" \"description\" \"hooks\" \"index\" \"info\" \"logs\" \"objects\" \"refs\" ", outstr.str());
    } else FAIL("The file directory is false\n");
 
 }
