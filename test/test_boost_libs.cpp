5d0d4ae26158c7c7f37a6213273ab99b333d6248
Bjorn Lindi <blindij@users.noreply.github.com>
2020-02-03 14:12:39 +0100

    Test Boost.Variant

--- test/test_boost_libs.cpp
+++ test/test_boost_libs.cpp
@@ -4,6 +4,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 #include <boost/any.hpp>
+#include <boost/variant.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -44,4 +45,22 @@ TEST_CASE("Get value form boost::any, second approach","[second]"){
     std::string* s2 = boost::any_cast<std::string>(&variable);
     myout << *s2;
     REQUIRE( myout.str() == "Hello world!" );
+}
+
+// -----------------------------------------------------------
+// Testing Boost::variant
+// -----------------------------------------------------------
+
+typedef boost::variant<int, const char*, std::string> the_var_t;
+
+TEST_CASE("Unrestricted unions using Boost.Variant","[union]"){
+    std::ostringstream myout;
+    std::vector<the_var_t> some_values;
+    some_values.push_back(10);
+    some_values.push_back("Hello there!");
+    some_values.push_back(std::string("Wow!"));
+    std::string& s = boost::get<std::string>(some_values.back());
+    s += " That is great!\n";
+    myout << s;
+    REQUIRE( myout.str() == "Wow! That is great!\n" );
 }
