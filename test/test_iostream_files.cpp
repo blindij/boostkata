4c1847514b62912f0ebf1a7eaab4459d4149b959
Bjorn Lindi <blindij@users.noreply.github.com>
2020-05-22 15:00:16 +0200

    Check for existence of file, add setup and cleanup
    - Check for existence of the gloser.txt file
    - Check for the size of the file log.txt
    - Create the file gloser.txt at setup
    - Remove the file at cleanup

--- test/test_iostream_files.cpp
+++ test/test_iostream_files.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 namespace io = boost::iostreams;
+namespace fs = boost::filesystem;
+
+TEST_CASE("Read from gloser.txt","[iostream][fileread]"){
+   string ps("gloser.txt");
+   REQUIRE(fs::exists(ps));
+}
 
 TEST_CASE("Write to log.txt","[iostream][log]"){
    string ps("log.txt");
@@ -12,5 +18,9 @@ TEST_CASE("Write to log.txt","[iostream][log]"){
    std::ostream out(&buf);
    // out writes to log.txt
    out << "Hello World!\n";
-   REQUIRE(boost::filesystem::exists(ps));
+   out.flush();
+   REQUIRE(boost::filesystem::file_size(ps) == 13);
+   SECTION("Remove log.txt file"){
+      REQUIRE(fs::remove(ps));
+   }
 }
