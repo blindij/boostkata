15bab49e8a7330bef687a5f367e7852ea23388ee
Bjorn Lindi <blindij@users.noreply.github.com>
2020-05-23 18:49:27 +0200

    Write line with comment. Remove comment.
    - a output filter remove a shell-style commment.

--- test/test_iostream_files.cpp
+++ test/test_iostream_files.cpp
@@ -1,17 +1,45 @@
 #include "catch2/catch.hpp"
 #include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
 #include <iostream>
 #include <boost/iostreams/stream.hpp>
 #include <boost/filesystem.hpp>
+#include <iostreams/example/container_device.hpp>
+#include "iostreams/example/shell_comments_filter.hpp"
+
 
 using namespace std;
 namespace io = boost::iostreams;
 namespace fs = boost::filesystem;
+namespace ex = boost::iostreams::example;
+
+TEST_CASE("Write line with a comment to file, removing the comment","[iostream][write_no_comment"){
+   string ps("nocomment.txt");
+   io::filtering_ostream out;
+   out.push(ex::shell_comments_output_filter());
+   out.push(io::file_sink(ps));
+   out << "This is a test with comment. # This text should be removed.\n";
+   out.flush();
+   REQUIRE(boost::filesystem::file_size(ps) == 30);
+}
 
 TEST_CASE("Test for existence of shell commented file","[iostream][shell_comment]"){
+   typedef ex::container_source<string> string_source;
+   string result;
    string ps("shell_comment.md");
    REQUIRE(fs::exists(ps));
+   SECTION("Read a line from shell commented file"){
+      io::stream_buffer<io::file_source> buf(ps);
+      istream                           in(&buf);
+      getline(in,result);
+      REQUIRE(result == "# Title in a Markdown file");
+//      SECTION("Remove shell comments"){
+//         string output;
+//         io::filtering_istream in;
+//         REQUIRE( in.push(ex::shell_comments_input_filter(result)) == EOF);
+//      }
+   }
 }
 
 TEST_CASE("Read from gloser.txt","[iostream][fileread]"){
