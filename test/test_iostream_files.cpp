3e57d81f8d7e5f4c991d71c3cd0ae27292a39b58
Bjorn Lindi <blindij@users.noreply.github.com>
2020-05-27 16:32:22 +0200

    Refactor
    - move testing of io filtering to own file
    - testing reading and writing to a file in own test file.

--- test/test_iostream_files.cpp
+++ test/test_iostream_files.cpp
@@ -1,88 +1,16 @@
+// File: test_iostream_files.cpp
+//
+// Test reading and writing to files with boost.iostream.file_sink and
+// boost.iostream.file_source
 #include "catch2/catch.hpp"
-#include <boost/iostreams/device/file.hpp>
-#include <boost/iostreams/device/file_descriptor.hpp>
-#include <boost/iostreams/filtering_stream.hpp>
-#include <iostream>
-#include <boost/iostreams/stream.hpp>
 #include <boost/filesystem.hpp>
 #include <iostreams/example/container_device.hpp>
-#include "iostreams/example/shell_comments_filter.hpp"
 
 
 using namespace std;
 namespace io = boost::iostreams;
 namespace fs = boost::filesystem;
-namespace ex = boost::iostreams::example;
 
-TEST_CASE("Write line with a comment to file, removing the comment","[iostream][write_no_comment"){
-   string ps("nocomment.txt");
-   io::filtering_ostream out;
-   out.push(ex::shell_comments_output_filter());
-   out.push(io::file_sink(ps));
-   out << "This is a test with comment. # This text should be removed.\n";
-   out.flush();
-   REQUIRE(boost::filesystem::file_size(ps) == 30);
-}
-
-TEST_CASE("Test for existence of shell commented file","[iostream][shell_comment]"){
-   typedef ex::container_source<string> string_source;
-   string result;
-   string ps("shell_comment.md");
-   REQUIRE(fs::exists(ps));
-   SECTION("Read a line from shell commented file"){
-      io::filtering_istream in;
-      in.push(ex::shell_comments_input_filter());
-      in.push(io::file_source(ps));
-      in >> result;
-      REQUIRE(result == "Test");
-      SECTION("Read rest of the lines from shell commented file"){
-         string tmp;
-         while( in  >> tmp ){
-           result += tmp;
-         }
-         REQUIRE( result == "TestThereisalwaysThetextusuallyrunsover");
-      }
-   }
-}
-
-TEST_CASE("Use get() with filter to remove shell-style comments","[iostream][get]"){
-   typedef ex::container_source<string> string_source;
-   char ch;
-   string result;
-   string ps("shell_comment.md");
-   REQUIRE(fs::exists(ps));
-   SECTION("Read a line from shell commented file"){
-      io::filtering_istream in;
-      in.push(ex::shell_comments_input_filter());
-      in.push(io::file_source(ps));
-      ch = in.get();
-      while(  ch != '\n'){
-         result += ch;
-         ch = in.get();
-      }
-      REQUIRE( result == "Test");
-   }
-}
-
-TEST_CASE("Use getline() with filter to remoe shell-style comments","[iostream][getline]"){
-   typedef ex::container_source<string> string_source;
-   char ch;
-   string result,newline;
-   string ps("shell_comment.md");
-   REQUIRE(fs::exists(ps));
-   SECTION("Read a line from shell commented file"){
-      io::filtering_istream in;
-      in.push(ex::shell_comments_input_filter());
-      in.push(io::file_source(ps));
-      getline(in, newline);
-      result += newline;
-      REQUIRE( result == "Test");
-      SECTION("Read the file using getline()"){
-         while( getline(in,newline)){result += newline;}
-         REQUIRE( result == "TestThere is always The text usually runs over" );
-      }
-   }
-}
 
 TEST_CASE("Read from gloser.txt","[iostream][fileread]"){
    // io::filtering_istream in;
