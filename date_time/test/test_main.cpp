6b44e13ae1e011f2ef002db610cbd01e07963f50
Bjorn Lindi <blindij@users.noreply.github.com>
2020-07-17 12:01:46 +0200

    Initial setup

---
+++ date_time/test/test_main.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+// ---- project tests in .hpp files 
+#include "test_project_scaffolding.hpp"
+
