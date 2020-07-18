c7ab315886e4bb04c9a24585f54b3774f09d7afc
Bjorn Lindi <blindij@users.noreply.github.com>
2020-07-18 17:29:16 +0200

    Convert string to integer (height)

--- date_time/test/test_project_scaffolding.hpp
+++ date_time/test/test_project_scaffolding.hpp
@@ -4,11 +4,28 @@
 // when using the Boost library
 #ifndef TEST_PROJECT_SCAFFOLDING_HPP
 #define TEST_PROJECT_SCAFFOLDING_HPP
+#include <cstdlib>
+#include <sstream>
 #include <boost/date_time/gregorian/gregorian_types.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
 
+using namespace std;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
+
+//boost::tuple<ptime,int> string2datetime_height(const string& s, string cm_height) {
+//   ptime t1(from_iso_string(s));
+//   int height = atoi((char)cm_height);
+//   return make_tuple(t1, height);
+//}
+
+TEST_CASE("Convert string to int","[date_time],[atoi]"){
+   istringstream h("63");
+   int f;
+   h >> f;
+   REQUIRE( f == 63);
+}
 
 TEST_CASE("Construct time object from ISO string","[date_time][isostring]"){
    ptime t1(date(2020,Jul,16),hours(2)+minutes(6));
@@ -38,4 +55,8 @@ TEST_CASE("Construct time object just after midnight","[date_time],[midnight]"){
    }
 }
 
+//TEST_CASE("Construct tuple with time object and int","[date_time],[tuple]"){
+//   boost::tuple<ptime,int> time_height_pair(ptime(date(2020,Jul,16),hours(2)+minutes(6)), 63);
+//   REQUIRE( time_height_pair == string2datetime_height("20200716T0206","63"));
+//}
 #endif // end TEST_PROJECT_SCAFFOLDING_HPP
